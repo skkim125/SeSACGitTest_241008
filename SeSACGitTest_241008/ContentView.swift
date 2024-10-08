6ca51573ca0105f1b669a132233a60b546368f47
skkim125 <kthanks125@gmail.com>
2024-10-08 14:16:59 +0900

    Init Project

---
+++ SeSACGitTest_241008/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeSACGitTest_241008
+//
+//  Created by 김상규 on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeSACGitTest_241008/SeSACGitTest_241008App.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACGitTest_241008App.swift
+//  SeSACGitTest_241008
+//
+//  Created by 김상규 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SeSACGitTest_241008App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
